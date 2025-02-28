e58ae3d56c7296be24b4789c36b7ae03edea8f64
Ajith UCA <ajith@unicornconnectedapps.com>
2025-02-28 20:40:33 +0530

    initial

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_CRUD_using_EF.MvcApplication" Language="C#" %>
---
+++ Models/ABCCompanyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD_using_EF.Models
+{
+    public class ABCCompanyDbContext : DbContext
+    {
+        public ABCCompanyDbContext() : base("ABC_DB_CS")
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
